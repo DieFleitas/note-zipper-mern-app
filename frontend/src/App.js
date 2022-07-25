f4feb78b065e1d9a54ecd0a6c5b5df5f8c5bdb58
DieFleitas <diegofleitas98@outlook.es>
2022-07-24 21:32:37 -0300

    react routerand my notes page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,22 @@
+import { BrowserRouter, Route } from "react-router-dom";
 import "./App.css";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import LandingPage from "./screens/LandingPage/LandingPage";
+import MyNotes from "./screens/MyNotes/MyNotes";
+
 
 function App() {
   return (
-    <>
-      <Header></Header>
+    <BrowserRouter>
+      <Header/>
       <main>
-        <LandingPage></LandingPage>
+        <Route path='/' compponent={LandingPage} exact/>
+        <Route path='/mynotes' compponent={() => <MyNotes/>} />
+
       </main>
       <Footer></Footer>
-    </>
+    </BrowserRouter>
   );
 }
 
