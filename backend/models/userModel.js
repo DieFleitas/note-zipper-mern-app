10b9ee50d888110a4deb419cdd7a423233e0c1af
DieFleitas <diegofleitas98@outlook.es>
2022-07-30 18:57:54 -0300

    back end done

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,5 +1,5 @@
-const mongoose = require("mongoose");
-const bcrypt = require("bcryptjs");
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
 
 const userSchema = mongoose.Schema(
   {
@@ -48,4 +48,4 @@ userSchema.pre("save", async function (next) {
 
 const User = mongoose.model("User", userSchema);
 
-module.exports = User;
+export default User;
